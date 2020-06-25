d1fa67deda8f6cad5d93478feb0f16901595c0d2
Nathan <nathankirley@gmail.com>
2020-06-25 13:36:19 +0100

    mainVillain and henchman tests written

--- src/test/java/HenchmanTest.java
+++ src/test/java/HenchmanTest.java
@@ -1,2 +1,38 @@
+import baddies.Henchman;
+import org.junit.Before;
+import org.junit.Test;
+import resources.Weapon;
+
+import static org.junit.Assert.assertEquals;
+
 public class HenchmanTest {
+    private Henchman henchman;
+    private Weapon weapon;
+
+    @Before
+    public void before(){
+        weapon = new Weapon("Bowler hat", 10);
+        henchman = new Henchman("Oddjob", weapon);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Oddjob", henchman.getName());
+    }
+
+    @Test
+    public void startsWith100Health(){
+        assertEquals(100, henchman.getHealth());
+    }
+
+    @Test
+    public void canLoseHealth(){
+        henchman.loseHealth();
+        assertEquals(90, henchman.getHealth());
+    }
+
+    @Test
+    public void hasWeapon(){
+        assertEquals("Bowler hat", henchman.getWeapon());
+    }
 }
--- src/test/java/MainBaddieTest.java
+++ src/test/java/MainBaddieTest.java
@@ -1,2 +1,31 @@
+import baddies.MainBaddie;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class MainBaddieTest {
+
+    private MainBaddie mainBaddie;
+
+    @Before
+    public void before(){
+        mainBaddie = new MainBaddie("Blofeld");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Blofeld", mainBaddie.getName());
+    }
+
+    @Test
+    public void startsWith100Health(){
+        assertEquals(100, mainBaddie.getHealth());
+    }
+
+    @Test
+    public void canLoseHealth(){
+        mainBaddie.loseHealth();
+        assertEquals(90, mainBaddie.getHealth());
+    }
 }
