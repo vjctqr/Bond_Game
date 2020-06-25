02cb9bfec4daedccc2b4023fa89138e9a1ad35d3
Nathan <nathankirley@gmail.com>
2020-06-25 13:49:06 +0100

    hasHenchmen test written

--- src/main/java/baddies/MainBaddie.java
+++ src/main/java/baddies/MainBaddie.java
@@ -1,9 +1,15 @@
 package baddies;
 
+import java.util.ArrayList;
+
 public class MainBaddie extends Villain{
+
+    private ArrayList<Henchman> henchmen;
 
     public MainBaddie(String name){
         super(name);
+        this.henchmen = new ArrayList<Henchman>();
     }
+
 }
 
--- src/test/java/MainBaddieTest.java
+++ src/test/java/MainBaddieTest.java
@@ -1,16 +1,37 @@
+import baddies.Henchman;
 import baddies.MainBaddie;
 import org.junit.Before;
 import org.junit.Test;
+import resources.Weapon;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
 public class MainBaddieTest {
 
     private MainBaddie mainBaddie;
+    private Henchman henchman1;
+    private Henchman henchman2;
+    private Henchman henchman3;
+    private Weapon weapon1;
+    private Weapon weapon2;
+    private Weapon weapon3;
+    private ArrayList<Henchman> henchmen;
 
     @Before
     public void before(){
+        weapon1 = new Weapon("Bowler hat", 10);
+        weapon2 = new Weapon("Teeth", 32);
+        weapon3 = new Weapon("Metal arm", 5);
+        henchman1 = new Henchman("Oddjob", weapon1);
+        henchman2 = new Henchman("Jaws", weapon2);
+        henchman3 = new Henchman("Teehee", weapon3);
         mainBaddie = new MainBaddie("Blofeld");
+        henchmen = new ArrayList<Henchman>();
+        Collections.addAll(henchmen, henchman1, henchman2, henchman3);
     }
 
     @Test
@@ -27,5 +48,10 @@ public class MainBaddieTest {
     public void canLoseHealth(){
         mainBaddie.loseHealth();
         assertEquals(90, mainBaddie.getHealth());
+    }
+
+    @Test
+    public void hasHenchmen(){
+        assertEquals(3, mainBaddie.getHenchmen());
     }
 }
