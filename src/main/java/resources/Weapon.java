10976cb8fce1e4e0768ed35298ea8d723066ee26
Nathan <nathankirley@gmail.com>
2020-06-25 11:29:46 +0100

    hasName and hasBullets tests written

--- src/main/java/resources/Weapon.java
+++ src/main/java/resources/Weapon.java
@@ -1,4 +1,13 @@
 package resources;
 
 public class Weapon {
+    private String name;
+    private int bullets;
+
+    public Weapon(String name, int bullets){
+        this.name = name;
+        this.bullets = bullets;
+    }
+
+    public String getName
 }
--- src/test/java/WeaponTest.java
+++ src/test/java/WeaponTest.java
@@ -1,9 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
 import resources.Weapon;
+
+import static org.junit.Assert.assertEquals;
 
 public class WeaponTest {
 
     private Weapon weapon;
 
+    @Before
+    public void before(){
+        weapon = new Weapon("Walther PPK", 6);
+    }
 
+    @Test
+    public void hasName(){
+        assertEquals("Walther PPK", weapon.getName());
+    }
+
+    @Test
+    public void hasBullets(){
+        assertEquals(6, weapon.getBullets());
+    }
 
 }
