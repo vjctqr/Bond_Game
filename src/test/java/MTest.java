87865e43d71453740d735c913dfc8fe9cb45afd7
Nathan <nathankirley@gmail.com>
2020-06-25 14:15:03 +0100

    hasAgents test written

--- src/test/java/MTest.java
+++ src/test/java/MTest.java
@@ -1,20 +1,37 @@
+import goodies.Agent;
 import goodies.M;
 import org.junit.Before;
 import org.junit.Test;
+import resources.Weapon;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
 public class MTest {
 
     private M m;
+    private Agent agent;
+    private Weapon weapon;
+    private ArrayList<Agent> agents;
 
     @Before
     public void before(){
-        m = new M("M");
+        weapon = new Weapon("Walther PPK", 6);
+        agent = new Agent("James Bond", weapon);
+        agents = new ArrayList<Agent>();
+        Collections.addAll(agents, agent);
+        m = new M("M", agents);
     }
 
     @Test
     public void hasName(){
         assertEquals("M", m.getName());
+    }
+
+    @Test
+    public void hasAgents(){
+        assertEquals(1, m.getAgentCount());
     }
 }
