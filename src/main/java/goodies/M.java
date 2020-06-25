1548c211f01feb8335219c0012700d365bd6f4d1
Nathan <nathankirley@gmail.com>
2020-06-25 14:16:52 +0100

    hasAgents test passes

--- src/main/java/goodies/M.java
+++ src/main/java/goodies/M.java
@@ -1,8 +1,21 @@
 package goodies;
 
+import java.util.ArrayList;
+
 public class M extends Mi6 {
 
-    public M(String name){
+    private ArrayList<Agent> agents;
+
+    public M(String name, ArrayList<Agent> agents){
         super(name);
+        this.agents = agents;
+    }
+
+    public ArrayList<Agent> getAgents(){
+        return this.agents;
+    }
+
+    public int getAgentCount(){
+        return this.agents.size();
     }
 }
