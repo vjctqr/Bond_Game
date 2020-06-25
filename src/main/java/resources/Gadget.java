14474016ed639069b99fd71881822797f708aadf
Nathan <nathankirley@gmail.com>
2020-06-25 11:42:43 +0100

    hasName and canBomb tests written

--- src/main/java/resources/Gadget.java
+++ src/main/java/resources/Gadget.java
@@ -1,4 +1,19 @@
 package resources;
 
 public class Gadget {
+    private String name;
+
+    public Gadget(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Str
 }
+
+
+
+
--- src/test/java/GadgetTest.java
+++ src/test/java/GadgetTest.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+import resources.Gadget;
+
+import static org.junit.Assert.assertEquals;
+
 public class GadgetTest {
+
+    private Gadget gadget;
+
+    @Before
+    public void before(){
+        gadget = new Gadget("Rolex");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Rolex", gadget.getName());
+    }
+
+    @Test
+    public void canBomb(){
+        assertEquals("Boom", gadget.explode());
+    }
+
+
+
 }
